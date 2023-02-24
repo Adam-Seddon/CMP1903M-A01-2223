36f23d1bda7e021e8399d4585d15da0fa95304f2
adams <adams@LAPTOP-9GVAIUF5>
2023-02-24 23:20:00 +0000

    Add project files.

---
+++ CMP1903M A01 2223/Pack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Pack
+    {
+        static List<Card> pack;
+
+        public Pack()
+        {
+            //Initialise the card pack here
+            pack = Enumerable.Range(1, 4)
+                .SelectMany(s => Enumerable.Range(1, 13)
+                                    .Select(c => new Card()
+                                    {
+                                        Suit = s,
+                                        Value = c
+        }
+                                            )
+                            )
+                   .ToList();
+
+                
+        }
+
+
+        /*0
+        public static bool shuffleCardPack(int typeOfShuffle)
+        {
+           //Shuffles the pack based on the type of shuffle
+
+        }*/
+        public static Card deal()
+        {
+            //Deals one
+            var card = pack.FirstOrDefault();
+            pack.Remove(card);
+
+            return card;
+        }
+        /*public static List<Card> dealCard(int amount)
+        {
+            //Deals the number of cards specified by 'amount'
+        }*/
+        
+    }
+}
---
+++ CMP1903M A01 2223/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Pack();
+            Console.WriteLine("hi");
+            Console.WriteLine("{0} a  {1}",Pack.deal().Suit,Pack.deal().Value);
+            Console.Read();
+        }
+    }
+}
